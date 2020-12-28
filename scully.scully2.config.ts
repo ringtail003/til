da9dce487f1212a96ce95826b6615129fbadcbd6
ringtail003 <ringtail003@gmail.com>
2020-12-29 08:30:52 +0900

    ng generate @scullyio/init:blog

--- scully.scully2.config.ts
+++ scully.scully2.config.ts
@@ -4,5 +4,11 @@ export const config: ScullyConfig = {
   projectName: "scully2",
   outDir: './dist/static',
   routes: {
+    '/blog/:slug': {
+      type: 'contentFolder',
+      slug: {
+        folder: "./blog"
+      }
+    },
   }
 };
