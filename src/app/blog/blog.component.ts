1f2317057e7d46c56996c078554bfef9e36255ec
ringtail003 <ringtail003@gmail.com>
2020-12-29 09:36:02 +0900

    add ScullyRoutesService

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,7 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {ActivatedRoute, Router, ROUTES} from '@angular/router';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router, ROUTES } from '@angular/router';
+import { ScullyRoute, ScullyRoutesService } from '@scullyio/ng-lib';
+import { Observable } from 'rxjs';
 
 declare var ng: any;
 
@@ -8,12 +10,18 @@ declare var ng: any;
   templateUrl: './blog.component.html',
   styleUrls: ['./blog.component.scss'],
   preserveWhitespaces: true,
-  encapsulation: ViewEncapsulation.Emulated
-
+  encapsulation: ViewEncapsulation.Emulated,
 })
 export class BlogComponent implements OnInit {
-  ngOnInit() {}
+  links$: Observable<ScullyRoute[]> = this.scully.available$;
 
-  constructor(private router: Router, private route: ActivatedRoute) {
+  ngOnInit() {
+    this.links$.subscribe((links) => console.log(links));
   }
+
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private scully: ScullyRoutesService
+  ) {}
 }
