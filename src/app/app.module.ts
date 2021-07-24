a21c5d970553011294b1f938b8f57a55a4071c3a
ringtail003 <ringtail003@gmail.com>
2021-07-24 22:27:20 +0900

    Collect components under components dir

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
 import { ScullyLibModule } from '@scullyio/ng-lib';
 import { ComponentModule } from 'src/app/components/module';
-import { TopComponent } from './top/top.component';
-import { TagComponent } from './components/tag/tag.component';
+import { TagComponent } from 'src/app/components/tag/tag.component';
+import { TopComponent } from 'src/app/components/top/top.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent, TopComponent, TagComponent],
