be72bbc3697feed887617481855c1697fd51d80e
ringtail003 <ringtail003@gmail.com>
2021-12-21 13:05:42 +0900

    eslintに変更

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  "root": true,
+  "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "parserOptions": { "project": ["./tsconfig.json"] },
+  "plugins": [
+      "@typescript-eslint"
+  ],
+  "rules": {
+  },
+  "ignorePatterns": ["src/**/*.test.ts", "src/frontend/generated/*"]
+};
