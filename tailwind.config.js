34c73c87c24e75a09d3473496572646af94179b2
ringtail003 <ringtail003@gmail.com>
2021-12-26 12:08:00 +0900

    デフォルトの青をskyに変更

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
+const colors = require('tailwindcss/colors');
+
 const baseConfig = {
   // mode: 'jit',
   content: ['./src/**/*.{html,ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: colors.sky,
+      }
+    }
   },
   variants: {
     extend: {},
