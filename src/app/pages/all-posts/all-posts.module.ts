628136d9c501c9073e4e91fbed0947b064546e3f
ringtail003 <ringtail003@gmail.com>
2022-01-10 17:23:10 +0900

    トップとlistを行き来するリンクの設置

--- src/app/pages/all-posts/all-posts.module.ts
+++ src/app/pages/all-posts/all-posts.module.ts
@@ -4,12 +4,13 @@ import { RouterModule } from '@angular/router';
 import { AllPostsRoutingModule } from 'src/app/pages/all-posts/all-posts-routing.module';
 import { AllPostsComponent } from 'src/app/pages/all-posts/all-posts.component';
 import { HeadingComponent } from 'src/app/pages/all-posts/components/heading/heading.component';
+import { IconModule } from 'src/app/shared/icons/icon.module';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
 
 const declarations = [AllPostsComponent, HeadingComponent, BlogPostComponent];
 
 @NgModule({
-  imports: [RouterModule, CommonModule, AllPostsRoutingModule],
+  imports: [RouterModule, CommonModule, AllPostsRoutingModule, IconModule],
   declarations,
 })
 export class AllPostsModule {}
