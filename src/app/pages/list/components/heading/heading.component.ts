e8d7ecf26aad2faa2a856653105d06aed84bb937
ringtail003 <ringtail003@gmail.com>
2022-01-10 12:51:13 +0900

    タイトル部分の調整

---
+++ src/app/pages/list/components/heading/heading.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'heading',
+  templateUrl: './heading.component.html',
+})
+export class HeadingComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/pages/list/list.module.ts
+++ src/app/pages/list/list.module.ts
@@ -1,12 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BlogIndexComponent } from 'src/app/pages/list/components/blog-index/blog-index.component';
+import { HeadingComponent } from 'src/app/pages/list/components/heading/heading.component';
 import { TagComponent } from 'src/app/pages/list/components/tag/tag.component';
 import { ListRoutingModule } from 'src/app/pages/list/list-routing.module';
 import { ListComponent } from 'src/app/pages/list/list.component';
 import { IconModule } from 'src/app/shared/icons/icon.module';
 
-const components = [BlogIndexComponent, ListComponent, TagComponent];
+const components = [
+  BlogIndexComponent,
+  ListComponent,
+  TagComponent,
+  HeadingComponent,
+];
 
 @NgModule({
   imports: [CommonModule, ListRoutingModule, IconModule],
