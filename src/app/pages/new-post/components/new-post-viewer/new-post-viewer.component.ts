109acebc5dce726f54be87a6c65dd6f016665e1b
ringtail003 <ringtail003@gmail.com>
2022-02-14 22:53:24 +0900

    パースしたHTMLを表示

--- src/app/pages/new-post/components/new-post-viewer/new-post-viewer.component.ts
+++ src/app/pages/new-post/components/new-post-viewer/new-post-viewer.component.ts
@@ -1,12 +1,27 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChange,
+} from '@angular/core';
+import { marked } from 'marked';
 import { BlogPost } from 'src/app/pages/new-post/models/blog-post';
 
 @Component({
   selector: 'new-post-viewer',
   templateUrl: './new-post-viewer.component.html',
 })
-export class NewPostViewerComponent implements OnInit {
+export class NewPostViewerComponent implements OnInit, OnChanges {
   @Input() blogPost!: BlogPost;
 
+  body!: string;
+
   ngOnInit(): void {}
+
+  ngOnChanges(changes: { blogPost?: SimpleChange }): void {
+    console.log(changes);
+
+    console.log(marked.parse(changes.blogPost?.currentValue.body));
+  }
 }
--- src/app/pages/new-post/new-post.component.ts
+++ src/app/pages/new-post/new-post.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BlogPost } from 'src/app/pages/new-post/models/blog-post';
+import * as Rx from 'rxjs';
 
 @Component({
   selector: 'new-post',
@@ -7,9 +8,14 @@ import { BlogPost } from 'src/app/pages/new-post/models/blog-post';
 })
 export class NewPostComponent implements OnInit {
   blogPost!: BlogPost;
+  body$ = new Rx.Subject<string>();
 
   ngOnInit(): void {
     this.blogPost = new BlogPost();
+
+    this.body$.pipe(Rx.debounceTime(500)).subscribe((value) => {
+      this.blogPost.body = value;
+    });
   }
 
   onChangeTitle($event: string): void {
@@ -17,7 +23,7 @@ export class NewPostComponent implements OnInit {
   }
 
   onChangeBody($event: string): void {
-    this.blogPost.body = $event;
+    this.body$.next($event);
   }
 
   onCreatePrButtonClick(): void {
